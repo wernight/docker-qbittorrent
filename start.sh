2f78fc160a77300893d5c19f9ac777ce4c2f5769
Werner Beroux <werner@beroux.com>
2015-06-23 14:03:50 +0200

    Allow arguments to be passed to wrapper script.

--- start.sh
+++ start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
+# Default configuration file
 if [ ! -f /config/qBittorrent.conf ]
 then
 	cp /default/qBittorrent.conf /config/qBittorrent.conf
 fi
 
 echo "Starting qbittorrent..."
-exec qbittorrent-nox
+exec qbittorrent-nox $*
